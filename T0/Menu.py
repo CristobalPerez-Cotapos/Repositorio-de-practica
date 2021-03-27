776e03fe34fab6dba3bbbe79c7df6341b30e8047
Cristobal <cprezcotapos@uc.cl>
2021-03-26 22:14:51 -0300

    progreso del viernes

---
+++ T0/Menu.py
@@ -0,0 +1,4 @@
+from Cargar_usuarios import cargar_usuarios, cargar_contactos
+from Relacionar_contactos import relacionar_contactos, diccionarios
+
+print(diccionarios("grupo"))
--- T0/Relacionar_contactos.py
+++ T0/Relacionar_contactos.py
@@ -1,9 +1,12 @@
 from Cargar_usuarios import cargar_usuarios, cargar_contactos
 
-for i in cargar_usuarios("usuarios.csv"):
-    for j in cargar_contactos("contactos.csv"):
-        if str(i) == j[0]:
-            i.agregar_contacto(j[1])                   # todos los contactos está relacionados
+
+def relacionar_contactos():
+    for i in cargar_usuarios("usuarios.csv"):
+        for j in cargar_contactos("contactos.csv"):
+            if str(i) == j[0]:
+                i.agregar_contacto(j[1])                   # todos los contactos está relacionados
+
 
 def diccionarios(tipo):                                 # tipo ¿regular o grupo?
     archivo_mensajes = open("mensajes.csv")
@@ -20,7 +23,7 @@ def diccionarios(tipo):                                 # tipo ¿regular o grupo
         if i[0] == "grupo":
             dic_mensajes_grupo[i[2]] = []
         else:
-            tupla_llave = (i[1],i[2])
+            tupla_llave = (i[1], i[2])
             dic_mensajes_regulares[tupla_llave] = []
 
     for i in lineas_archivo_mensajes:                   # ahora rellenamos esas entradas
@@ -28,15 +31,17 @@ def diccionarios(tipo):                                 # tipo ¿regular o grupo
         for j in i[4:]:                                  # reconstruimos los mensajes que
             if len(mensaje) != 0:                       # contenian comas y destruimos al hacer split
                 mensaje = mensaje + ", " + j
+            else:
+                mensaje = j
+        mensaje = mensaje.strip("\n")
 
         if i[0] == "grupo":
             tupla_mensaje_grupo = (i[1], i[3], mensaje)           # contenido -> (emisor,hora,mensaje)
             dic_mensajes_grupo[i[2]].append(tupla_mensaje_grupo)
         else:
-            tupla_mensaje_regular = (i[3],mensaje)
-            dic_mensajes_regulares[i[1],i[2]].append(tupla_mensaje_regular)    #contenido -> (hora, mensaje)
+            tupla_mensaje_regular = (i[3], mensaje)
+            dic_mensajes_regulares[i[1], i[2]].append(tupla_mensaje_regular)    # contenido -> (hora, mensaje)
     if tipo == "grupo":
         return dic_mensajes_grupo
     else:
         return dic_mensajes_regulares
-
